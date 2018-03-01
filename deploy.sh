c7b57f0b9a644bb68b88f4eb8b58d5eb8b391bc5
Kirill Klenski, Patrick Abner <kirill@codeborne.com, patrick@codeborne.com>
2018-03-01 16:43:30 +0200

    155617251 add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -x
+
+JENKINS_WORKSPACE="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cd /home/commonsos
+
+[ -d logs ] || mkdir -p logs
+
+rm -rf commonsos-api
+mkdir -p commonsos-api
+unzip $JENKINS_WORKSPACE/build/distributions/commonsos-api.zip
+
+killall java || echo "No previous instance running"
+
+pushd commonsos-api
+java -Xmx1024m -Dmode=production -Dfile.encoding=UTF-8 -jar commonsos-api.jar  >> ../logs/stdouterr.log 2>&1 &
+popd
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/bin/bash
 java -Xmx1024m -Dmode=test -Dfile.encoding=UTF-8 -jar commonsos-api.jar >> ../logs/stdouterr.log 2>&1 &
