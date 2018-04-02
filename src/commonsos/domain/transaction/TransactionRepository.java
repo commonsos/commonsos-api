726e45a57c3b77182de941ee72ac64f051bfcf5b
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-04-02 15:46:42 +0300

    156369118 add some mock data

--- src/commonsos/domain/transaction/TransactionRepository.java
+++ src/commonsos/domain/transaction/TransactionRepository.java
@@ -2,13 +2,19 @@ package commonsos.domain.transaction;
 
 import commonsos.domain.auth.User;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.time.OffsetDateTime.now;
+import static java.time.temporal.ChronoUnit.HOURS;
 import static java.util.stream.Collectors.toList;
 
 public class TransactionRepository {
-  public List<Transaction> transactions = new ArrayList<>();
+  public List<Transaction> transactions = new ArrayList<Transaction>() {{
+    add(new Transaction().setBeneficiaryId("worker").setRemitterId("elderly1").setAmount(BigDecimal.TEN).setCreatedAt(now().minus(1, HOURS)));
+    add(new Transaction().setBeneficiaryId("elderly1").setRemitterId("worker").setAmount(BigDecimal.ONE).setCreatedAt(now()));
+  }};
 
   public void create(Transaction transaction) {
     transactions.add(transaction);
