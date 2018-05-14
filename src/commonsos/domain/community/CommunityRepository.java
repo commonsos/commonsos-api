c26ff25b0897809b17ee40e9a1107862fd74ec34
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-05-14 11:39:07 +0300

    156564646 make repository Guice-compatible

--- src/commonsos/domain/community/CommunityRepository.java
+++ src/commonsos/domain/community/CommunityRepository.java
@@ -3,13 +3,17 @@ package commonsos.domain.community;
 import commonsos.EntityManagerService;
 import commonsos.Repository;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.List;
 import java.util.Optional;
 
 import static java.util.Optional.ofNullable;
 
+@Singleton
 public class CommunityRepository extends Repository {
 
+  @Inject
   public CommunityRepository(EntityManagerService entityManagerService) {
     super(entityManagerService);
   }
