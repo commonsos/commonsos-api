0eb22d9bc7eaa736fa2c696d95875619fc27dd8e
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-05-15 13:10:55 +0300

    156564646 remove unused fields

--- src/commonsos/domain/blockchain/TokenERC20Deployer.java
+++ src/commonsos/domain/blockchain/TokenERC20Deployer.java
@@ -10,15 +10,9 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.File;
-import java.math.BigInteger;
 import java.util.Scanner;
 
-import static java.math.BigInteger.ONE;
-
 public class TokenERC20Deployer {
-
-  public static final BigInteger INITIAL_TOKEN_AMOUNT = new BigInteger("2").pow(256).subtract(ONE);
-  public static final BigInteger TOKEN_DEPLOYMENT_GAS_LIMIT = new BigInteger("262568");
 
   public static void main(String[] args) throws Exception {
     if (args.length < 1) {
