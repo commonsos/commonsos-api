77cbea639b08732ae7fa67b60701496f95cf4197
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-07-05 11:01:53 +0300

    remove mode variable as it influences only logging and any value other than 'developlment' is ok

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 [ -d ../logs ] || mkdir -p ../logs
 
 . ~/.local_environment
-java -Xmx1024m -Dmode=test -Dfile.encoding=UTF-8 -jar commonsos-api.jar >> ../logs/stdouterr.log 2>&1 &
+java -Xmx1024m -Dfile.encoding=UTF-8 -jar commonsos-api.jar >> ../logs/stdouterr.log 2>&1 &
 
 echo $! > pid
 echo "Started pid $!"
