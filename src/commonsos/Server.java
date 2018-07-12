7029376013191c2b56588ec4647c1f8324f188cc
Kirill Klenski, Tõnis Aruste <kirill@codeborne.com, tonis@codeborne.com>
2018-07-12 13:48:34 +0300

    158968602 temporary disable CSRF protection as it doesn't work on Cordova/iOS

--- src/commonsos/Server.java
+++ src/commonsos/Server.java
@@ -55,7 +55,7 @@ public class Server {
 
     before(new LogFilter());
     before((request, response) -> log.info(requestInfo(request)));
-    before(new CSRFFilter(asList("/login", "/logout", "/create-account")));
+//    before(new CSRFFilter(asList("/login", "/logout", "/create-account")));
     before(new AuthenticationFilter(asList("/login", "/logout", "/create-account", "/communities")));
 
     post("/login", injector.getInstance(LoginController.class), toJson);
